e9c2ffb641469bd7b71d3d424e71345f6bd7e967
ubuntu0914 <ubuntu0914@gmail.com>
2020-03-22 19:12:15 +0900

    hotfix브랜치에서 Variable1.java추가함.

---
+++ dev_talk/src/variable/hotfix/Variable1.java
@@ -0,0 +1,10 @@
+package variable.hotfix;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("변수 테스트 Variable1.java-hotfix브랜치에서 추가함.");
+	}
+
+}
